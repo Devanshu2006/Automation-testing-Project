ee1fa48b34215b73bea8e3d2f4bb39bf5fd9ae7b
Devanshu Girare <devanshupawar20@gmail.com>
2026-09-13 10:45:01 +0530

    fixing the errors

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
 import os
 
-API_BASE_URL = os.getenv(
-    "API_BASE_URL",
-    "https://jsonplaceholder.typicode.com"
-)
+ENV = os.getenv("TEST_ENV", "qa")
 
+ENVIRONMENTS = {
+    "dev": {
+        "api_base_url": "https://jsonplaceholder.typicode.com"
+    },
+    "qa": {
+        "api_base_url": "https://jsonplaceholder.typicode.com"
+    },
+    "staging": {
+        "api_base_url": "https://jsonplaceholder.typicode.com"
+    }
+}
+
+API_BASE_URL = ENVIRONMENTS[ENV]["api_base_url"]
 API_TOKEN = os.getenv("API_TOKEN")
